5c3387e25fa34579b7e8e333f109a3982ad9889f
farah <farah9hias@gmail.com>
2022-06-03 21:24:15 -0700

    push to heroku

--- server.js
+++ server.js
@@ -7,14 +7,14 @@ const notes = require('./db/db.json');
 
 
 
-const PORT = 3001;
+const PORT = process.env.PORT || 3001;
 
 const app = express();
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-app.use(express.static('public'));
+app.use(express.static('public'));``
 
 app.get('/', (req, res) =>
   res.sendFile(path.join(__dirname, '/public/index.html'))
